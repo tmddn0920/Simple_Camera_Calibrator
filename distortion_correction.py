902c128ca39e73a8544be11312cf757c6f84a9c7
Seung._.rain <likepiano920@naver.com>
2025-04-08 06:35:21 +0900

    Add Codes and Screenshots and README.md

---
+++ distortion_correction.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2 as cv
+
+video_file = 'chessboard.mov'
+
+# The given video and calibration data
+K = np.array([
+    [887.66457342, 0, 959.47532409],
+    [0, 893.84894272, 522.79319593],
+    [0, 0, 1]
+])
+dist_coeff = np.array([
+    [0.02874492, -0.05810351, -0.00472185, -0.00033239, 0.07060768]
+])
+
+# Open a video
+video = cv.VideoCapture(video_file)
+assert video.isOpened(), 'Cannot read the given input, ' + video_file
+
+# Run distortion correction
+show_rectify = True
+map1, map2 = None, None
+while True:
+    # Read an image from the video
+    valid, img = video.read()
+    if not valid:
+        break
+
+    # Rectify geometric distortion (Alternative: `cv.undistort()`)
+    info = "Original"
+    if show_rectify:
+        if map1 is None or map2 is None:
+            map1, map2 = cv.initUndistortRectifyMap(K, dist_coeff, None, None, (img.shape[1], img.shape[0]), cv.CV_32FC1)
+        img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR)
+        info = "Rectified"
+    cv.putText(img, info, (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
+
+    # Show the image and process the key event
+    cv.imshow("Geometric Distortion Correction", img)
+    key = cv.waitKey(10)
+    if key == ord(' '):     # Space: Pause
+        key = cv.waitKey()
+    if key == 27:           # ESC: Exit
+        break
+    elif key == ord('\t'):  # Tab: Toggle the mode
+        show_rectify = not show_rectify
+
+video.release()
+cv.destroyAllWindows()
